29d8ab85e9d7ace541caf3c6d23fbe1092ff49dc
Harsh Morayya <harshmorayya@ymail.com>
2023-02-23 09:17:25 +0530

    Update LeagueWebsiteApplication.java

--- league-website-backend/src/main/java/com/woxsen/leaguewebsite/LeagueWebsiteApplication.java
+++ league-website-backend/src/main/java/com/woxsen/leaguewebsite/LeagueWebsiteApplication.java
@@ -8,7 +8,6 @@ public class LeagueWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeagueWebsiteApplication.class, args);
-		System.out.println("");
 	}
 
 }
